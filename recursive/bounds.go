e59300fc10a20c6aadbe1327498bcd9b64ad7fa3
Vlada <sakac1990@gmail.com>
2020-12-15 23:10:45 +0100

    Recursive searcher with shrinking bounds implementation

---
+++ recursive/bounds.go
@@ -0,0 +1,44 @@
+package recursive
+
+import (
+	"binary-chop/common"
+	"fmt"
+)
+
+// BoundsBinarySearcher uses tail recursive method for binary tree traversion
+// by shrinking the array bounds until element is found
+type BoundsBinarySearcher struct{}
+
+// Search is the tail recursive bounds implementation of the binary searcher
+func (bbs BoundsBinarySearcher) Search(num int, tree []int) (int, error) {
+	// we only need to check if the tree is valid once, no need to check this in the recursion
+	valid, err := common.ValidateArray(num, tree)
+	if err != nil {
+		return -1, fmt.Errorf("tree is not valid")
+	}
+
+	if !valid {
+		return -1, nil
+	}
+
+	return bbs.shrinkAndFind(num, 0, len(tree), tree), nil
+}
+
+func (bbs BoundsBinarySearcher) shrinkAndFind(num, min, max int, tree []int) int {
+	// end of the recursion, if element is not found, return -1
+	if min > max {
+		return -1
+	}
+
+	mid := int((min + max) / 2)
+	// if mid element is equal to `num` return mid index
+	if tree[mid] == num {
+		return mid
+	} else if tree[mid] > num {
+		//  if mid element is greater than the `num`, shrink array bounds and search again
+		return bbs.shrinkAndFind(num, min, mid-1, tree)
+	}
+
+	// `num` is lower than mid element, shrink array bounds and search again
+	return bbs.shrinkAndFind(num, mid+1, max, tree)
+}
---
+++ recursive/bounds_test.go
@@ -0,0 +1,87 @@
+package recursive
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestBoundsBinarySearcher(t *testing.T) {
+	searcher := BoundsBinarySearcher{}
+
+	t.Run("empty array", func(t *testing.T) {
+		tree := []int{}
+		result, err := searcher.Search(3, tree)
+		assert.NoError(t, err)
+		assert.Equal(t, -1, result)
+	})
+
+	t.Run("unordered array", func(t *testing.T) {
+		tree := []int{1, 5, 3, 4}
+		_, err := searcher.Search(3, tree)
+		assert.Error(t, err)
+	})
+
+	t.Run("lowest value mismatch", func(t *testing.T) {
+		tree := []int{4, 6, 7, 8}
+		result, err := searcher.Search(1, tree)
+		assert.NoError(t, err)
+		assert.Equal(t, -1, result)
+	})
+
+	t.Run("highest value mismatch", func(t *testing.T) {
+		tree := []int{4, 6, 7, 8}
+		result, err := searcher.Search(9, tree)
+		assert.NoError(t, err)
+		assert.Equal(t, -1, result)
+	})
+
+	t.Run("one element array miss", func(t *testing.T) {
+		tree := []int{4}
+		result, err := searcher.Search(1, tree)
+		assert.NoError(t, err)
+		assert.Equal(t, -1, result)
+	})
+
+	t.Run("one element array hit", func(t *testing.T) {
+		tree := []int{4}
+		result, err := searcher.Search(4, tree)
+		assert.NoError(t, err)
+		assert.Equal(t, 0, result)
+	})
+
+	t.Run("even index value", func(t *testing.T) {
+		tree := []int{4, 5, 8, 9, 12, 15}
+		result, err := searcher.Search(12, tree)
+		assert.NoError(t, err)
+		assert.Equal(t, 4, result)
+	})
+
+	t.Run("odd index value", func(t *testing.T) {
+		tree := []int{4, 5, 8, 9, 12, 15}
+		result, err := searcher.Search(5, tree)
+		assert.NoError(t, err)
+		assert.Equal(t, 1, result)
+	})
+
+	t.Run("last index value", func(t *testing.T) {
+		tree := []int{4, 5, 8, 9, 12, 15}
+		result, err := searcher.Search(15, tree)
+		assert.NoError(t, err)
+		assert.Equal(t, 5, result)
+	})
+
+	t.Run("first index value", func(t *testing.T) {
+		tree := []int{4, 5, 8, 9, 12, 15}
+		result, err := searcher.Search(4, tree)
+		assert.NoError(t, err)
+		assert.Equal(t, 0, result)
+	})
+
+	t.Run("element not found", func(t *testing.T) {
+		tree := []int{4, 5, 8, 9, 12, 15}
+		result, err := searcher.Search(7, tree)
+		assert.NoError(t, err)
+		assert.Equal(t, -1, result)
+	})
+}
