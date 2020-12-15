b3bc4df5546f5d9a57b552327d742396af337bb4
Vlada <sakac1990@gmail.com>
2020-12-15 23:21:24 +0100

    TDD approach to the loop chop implementation of binary searcher

---
+++ loop/chop.go
@@ -0,0 +1,10 @@
+package loop
+
+// ChopBinarySearcher uses loop search method gor binary search traversion
+// by chopping the array in every iteration until element is found
+type ChopBinarySearcher struct{}
+
+// Search is the loop  chop implementation of th binary searcher
+func (cbs ChopBinarySearcher) Search(num int, tree []int) (int, error) {
+	return -1, nil
+}
